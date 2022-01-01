3a70818887287a2330c261e96dcf4d8448fbf75e
Karina Klevtsova <88926633+kca2rie@users.noreply.github.com>
2022-01-01 22:51:50 +0300

    Update firstProject.py

--- firstProject.py
+++ firstProject.py
@@ -559,3 +559,43 @@ h1.set_author("")
 print(h1.get_author())
 h1.set_price("")
 print(h1.get_price())
+
+
+# 26/12/21
+# Задача. Разработать класс Sphere для представления сферы в трехмерном пространстве. Создать в классе набор методов.
+
+from math import pi
+
+
+class Sphere:
+    def __init__(self, *arg):
+        if len(arg) == 0:
+            arg = (1, 0, 0, 0)
+        elif len(arg) == 1:
+            arg = (arg[0], 0, 0, 0)
+        else:
+            raise TypeError
+        self.r, self.x, self.y, self.z = arg
+
+    def get_volume(self):
+        return (self.r ** 3) * pi * 4 / 3
+
+    def get_square(self):
+        return (self.r ** 2) * pi * 4
+
+    def get_radius(self):
+        return self.r
+
+    def get_center(self):
+        return (self.x, self.y, self.z)
+
+    def set_radius(self, r):
+        self.r = r
+
+    def set_center(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def is_point_inside(self, x, y, z):
+        return (self.x - x) ** 2 + (self.y - y) ** 2 + (self.z - z) ** 2 < self.r ** 2
