52fb4e0f297c76b8253436ee5f3ea0d6e21691a3
Karina Klevtsova <88926633+kca2rie@users.noreply.github.com>
2022-01-28 23:02:09 +0300

    Update firstProject.py

--- firstProject.py
+++ firstProject.py
@@ -760,30 +760,49 @@ print(a.strength)
 # Реализуйте производные классы Линейное уравнение и Квадратное уравнение с собственными методами вычисления корней и вывода на экран.
 
  from abc import ABC, abstractmethod
+    
+    
+class Root(ABC):
+    def __init__(self, val_1, val_2):
+        self.val_1 = val_1
+        self.val_2 = val_2
 
- class Root(ABC):
-     def __init__(self, value):
-         self.value = value
+    def calculate_linear_equation(self):
+        root = -self.val_2 / self.val_1
+        return root
 
-     @abstractmethod
-     def calculation_of_roots(self):
-         pass
+    def quad_equation(self):
+        disc = self.val_2 * 2 + 4 * self.val_1 * 3
+        root_1 = (self.val_2 + math.sqrt(disc)) / (2 * self.val_1)
+        root_2 = (self.val_2 - math.sqrt(disc)) / (2 * self.val_1)
+        return root_1, root_2
 
-      def print_value(self):
-          print(self.value, end=" ")
-
- class Linear(Root):
-     def calc1(self):
-         pass
+    @abstractmethod
+    def print_info(self):
+        raise NotImplementedError
 
 
- class Square(Root):
-     def calc2(self):
-         pass
+class Linear(Root):
+    def __init__(self, val_1, val_2):
+        super().__init__(val_1, val_2)
 
- cl = Linear()
- cl.calc1()
- print()
+    def print_info(self):
+        print(f"The root of {self.val_1}x+{self.val_2}=0 is: {round(self.calculate_linear_equation(), 2)}")
+
+
+class Quad(Root):
+    def __init__(self, val_1, val_2):
+        super().__init__(val_1, val_2)
+
+    def print_info(self):
+        print(f"The roots of {self.val_1}x^2-{self.val_2}x-3=0 are: {self.quad_equation()}")
+
+
+l = Linear(3, 7)
+l.print_info()
+
+w = Quad(1, 2)
+w.print_info()
 
 
 # 22.01
