4b183cec8a21450d743841eb4acbdc7592617ad5
Karina Klevtsova <88926633+kca2rie@users.noreply.github.com>
2022-01-31 23:15:15 +0300

    Update firstProject.py

--- firstProject.py
+++ firstProject.py
@@ -825,3 +825,78 @@ class Student(ABC):
 name = Student('Roman')
 laptop = name.Laptop('HP', 'i7', '16')
 print()
+
+
+# 23.01
+class Clock:
+    __DAY = 86400
+
+    def __init__(self, secs: int):
+        if not isinstance(secs, int):
+            raise ValueError("Секунды должны быть целым числом")
+        self.secs = secs % self.__DAY
+
+    def get_format_time(self):
+        s = self.secs % 60  # секунды
+        m = (self.secs // 60) % 60  # минуты
+        h = (self.secs // 3600) % 24  # часы
+        return f"{Clock.__get_form(h)}:{Clock.__get_form(m)}:{Clock.__get_form(s)}"
+
+    @staticmethod
+    def __get_form(x):
+        return str(x) if x > 9 else "0" + str(x)
+
+    def __add__(self, other):
+        if not isinstance(other, Clock):
+            raise AttributeError("Правый операнд должен быть типом данных Clock")
+        return Clock(self.secs + other.secs)
+
+    def __eq__(self, other):
+        return self.secs == other.secs
+
+    def __ne__(self, other):
+        return  not self.__eq__(other)
+
+    def __le__(self,other):
+        return self.secs <= other.secs
+
+    def __lt__(self, other):
+        return self.secs < other.secs
+
+    def __gt__(self, other):
+        return self.secs > other.secs
+
+    def __ge__(self, other):
+        return self.secs >= other.secs
+
+    def __getitem__(self, item):
+        if not isinstance(item, str):
+            raise ValueError("Ключ должен быть строкой")
+        if item == "hour":
+            return (self.secs // 3600) % 24
+        elif item == "min":
+            return (self.secs // 60) % 60
+        elif item == "sec":
+            return self.secs % 60
+        return "Неверный ключ"
+
+    def __setitem__(self, key, value):
+        if not isinstance(key, value):
+            raise ValueError("Ключ должен быть строкой")
+        if not isinstance(value, int):
+            raise ValueError("Значение должно быть целым числом")
+        s = self.secs % 60
+        m = (self.secs // 60) % 60
+        h = (self.secs // 3600) % 24
+        if key == "hour":
+            self.secs = s + 60 *m + value * 3600
+        elif key == "min":
+            self.secs = s + 60 * value + h * 3600
+        elif key == "sec":
+            self.secs = value + 60 * m + h * 3600
+
+
+c1 = Clock(80000)
+print(c1.get_format_time())
+c1["hour"] = 10
+print(c1["hour"], c1["min"], c1["sec"])
