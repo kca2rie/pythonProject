c432ab3e73b89b5a1b227e6e93046988b6e1e932
Karina Klevtsova <88926633+kca2rie@users.noreply.github.com>
2022-03-01 18:25:41 +0300

    Update controller.py

--- controller.py
+++ controller.py
@@ -9,3 +9,8 @@ class Controller:
         answer = None
         while answer != 'q':
             answer = self.user_interface.wait_user_answer()
+            self.check_user_answer(answer)
+            
+    def check_user_answer(self, answer):
+        if answer == "1":
+            film = self.user_interface.add_user_film()
