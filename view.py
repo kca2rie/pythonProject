f2730cf36bb656e35f05aebec7e6dc346cfccb82
Karina Klevtsova <88926633+kca2rie@users.noreply.github.com>
2022-03-01 18:22:29 +0300

    Update view.py

--- view.py
+++ view.py
@@ -1,3 +1,8 @@
 class UserInterface:
-    def wait_user_answer:
-      pass
+    def wait_user_answer(self):
+      print(" Редактирование данных католога фильмов ".center(50, "="))
+      print("Действия с фильмами")
+      print("q - выход из программы")
+      user_answer = input("Выберите вариант действия: ")
+      print("=" * 50)
+      return user_answer
