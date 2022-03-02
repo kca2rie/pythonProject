4376c13e6a67d56e3668abd1ba88daf78cacf1d3
Karina Klevtsova <88926633+kca2rie@users.noreply.github.com>
2022-03-02 18:42:09 +0300

    Update view.py

--- view.py
+++ view.py
@@ -1,31 +1,41 @@
+def add_title(title):
+    def wrapper(func):
+        def wrap(*args, **kwargs):
+            print(f" {title} ".center(50, "="))
+            output = func(*args, **kwargs)
+            print("=" * 50)
+            return output
+        return wrap
+    
+    return wrapper
+
+
 class UserInterface:
+    @add_title('Ввод пользовательских данных')
     def wait_user_answer(self):
-      print(" Редактирование данных католога фильмов ".center(50, "="))
-      print("Действия с фильмами")
-      print("1 - добавление фильма")
-      print("q - выход из программы")
+        print("Действия с фильмами:")
+        print("1 - добавление фильма"
+            "2 - каталог фильмов"
+            "q - выход из программы")
       user_answer = input("Выберите вариант действия: ")
-      print("=" * 50)
       return user_answer
     
-   def add_user_film(self):
-      dict_film = {
-          "название" : None,
-          "жанр": None,
-          "режиссёр": None,
-          "год выпуска": None,
-          "длительность": None,
-          "студия": None,
-          "актёры": None
+    @add_title('Добавление фильма:')
+    def add_user_film(self):
+        dict_film = {
+            "название" : None,
+            "жанр": None,
+            "режиссёр": None,
+            "год выпуска": None,
+            "длительность": None,
+            "студия": None,
+            "актёры": None
       } 
-        print("Добавление фильма: ".center(50, "="))
         for key in dict_film:
             dict_film[key] = input(f"Введите {key} фильма: ")
-        print("=" * 50)
         return dict_film
     
+    @add_title('Каталог фильмов:')
     def show_all_films(self, films):
-        print("Список фильмов: ".center(50, "="))
         for ind, film in enumerate(films, start=1):
             print(f"{ind}. {films}")
-        print("=" * 50)
