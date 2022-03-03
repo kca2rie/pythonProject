052ec77b0d8fc2588dc97db09d0731da7bf86933
Karina Klevtsova <88926633+kca2rie@users.noreply.github.com>
2022-03-03 22:11:09 +0300

    Update model.py

--- model.py
+++ model.py
@@ -1,3 +1,6 @@
+import pickle
+import os.path
+
 class Film:
     def __init__(self, title, genre, director, year, duration, studio, actors):
         self.title = title
@@ -13,7 +16,8 @@ class Film:
         
 class FilmModel:
     def __init__(self):
-        self.films = {}
+        self.db_name = "db.txt"
+        self.films = self.load_data()
         
     def add_film(self, dict_film):
         film = Film(*dict_film.values())
@@ -38,3 +42,14 @@ class FilmModel:
     
     def remove_film(self, user_title):
         returm self.films.pop(user_title)
+        
+    def load_data(self):
+        if os.path.exists(self.db_name):
+            with open(self.db_name, "rb") as f:
+                return pickle.load(f)
+        else:
+            return dict()
+        
+    def save_data(self):
+        with open(self.db_name, "wb") as f:
+            pickle.dump(self.films, f)
